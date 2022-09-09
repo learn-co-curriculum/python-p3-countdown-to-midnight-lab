3640c87900c79fc10a0f86af2c7ebee8fa206b5a
Ben Botsford <ben.botsford@flatironschool.com>
2022-09-09 14:53:26 -0500

    add tests

--- lib/countdown.py
+++ lib/countdown.py
@@ -1,18 +1,1 @@
-from time import sleep
-
-def countdown(number):
-
-    while number:
-        print(f'{number} SECOND(S)!')
-        number -= 1
-
-    print('HAPPY NEW YEAR!')
-
-def countdown_with_sleep(number):
-
-    while number:
-        print(f'{number} SECOND(S)!')
-        number -= 1
-        sleep(1)
-
-    print('HAPPY NEW YEAR!')
+# your code goes here!
--- lib/testing/countdown_test.py
+++ lib/testing/countdown_test.py
@@ -6,26 +6,23 @@ from countdown import countdown, countdown_with_sleep
 class TestCountdown:
     '''Function countdown() in countdown.py'''
 
-    def test_(self):
-        '''prints the argument passed to it.'''
+    def test_counts_down_prints_happy_new_year(self):
+        '''counts down from number and prints "HAPPY NEW YEAR!"'''
         captured_out = io.StringIO()
         sys.stdout = captured_out
-        parrot("Hello!")
+        countdown(5)
         sys.stdout = sys.__stdout__
-        assert(captured_out.getvalue() == "Hello!\n")
+        assert(captured_out.getvalue() == "5 SECOND(S)!\n4 SECOND(S)!\n" + \
+            "3 SECOND(S)!\n2 SECOND(S)!\n1 SECOND(S)!\nHAPPY NEW YEAR!\n")
 
-    def test_returns_argument(self):
-        '''returns the argument passed to it.'''
-        assert(parrot("Hello!") == "Hello!")
+class TestCountdownWithSleep:
+    '''Function countdown_with_sleep() in countdown.py'''
 
-    def test_prints_squawk_by_default(self):
-        '''prints "Squawk!" if no argument is passed.'''
+    def test_counts_down_prints_happy_new_year(self):
+        '''counts down from number and prints "HAPPY NEW YEAR!"'''
         captured_out = io.StringIO()
         sys.stdout = captured_out
-        parrot()
+        countdown_with_sleep(5)
         sys.stdout = sys.__stdout__
-        assert(captured_out.getvalue() == "Squawk!\n")
-
-    def test_returns_squawk_by_default(self):
-        '''returns "Squawk!" if no argument is passed.'''
-        assert(parrot() == "Squawk!")
+        assert(captured_out.getvalue() == "5 SECOND(S)!\n4 SECOND(S)!\n" + \
+            "3 SECOND(S)!\n2 SECOND(S)!\n1 SECOND(S)!\nHAPPY NEW YEAR!\n")
