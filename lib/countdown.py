23070592d9aa24946b6cd3322d852c6bb2b75c60
Ben Botsford <ben.botsford@flatironschool.com>
2022-09-09 14:53:46 -0500

    add solution

--- lib/countdown.py
+++ lib/countdown.py
@@ -1,1 +1,18 @@
-# your code goes here!
+from time import sleep
+
+def countdown(number):
+
+    while number:
+        print(f'{number} SECOND(S)!')
+        number -= 1
+
+    print('HAPPY NEW YEAR!')
+
+def countdown_with_sleep(number):
+
+    while number:
+        print(f'{number} SECOND(S)!')
+        number -= 1
+        sleep(1)
+
+    print('HAPPY NEW YEAR!')
